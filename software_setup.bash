d97c8190dc9d492e6e147c20aac7a889fde9a75b
AutonomousHansen <halexhansen@gmail.com>
2020-07-06 13:31:00 -0400

    Initial commit

---
+++ software_setup.bash
@@ -0,0 +1,8 @@
+# Installing brew package manager
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Installing Handbrake video/audio encoder
+brew install handbrake
+
+#Installing ffmpeg command-line audio encoder
+brew install ffmpeg
