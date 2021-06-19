d82ac40ad87cb68f55d1e42ca9f847165e34b2ee
Hunter <hansena@mit.edu>
2021-06-19 14:29:03 -0400

    Better processing of trailing numerical values

--- cb_to_film.py
+++ cb_to_film.py
@@ -26,13 +26,14 @@ def main():
     files = [f for f in os.listdir(os.getcwd()) if fnmatch.fnmatch(f, '[!_]*')]
     for f in files:
         try:
-            inst = re.findall(r'(?!=)([a-z1-9]*)(?=.wav)', f)[0].translate(None, string.digits)
+            inst = re.findall(r'(?!=)([a-z1-9]*)(?=.wav)', f)[0].strip(string.digits)
             for keys, abbr in abbr_dict.items():
                 if abbr in f:
                     print ("Named already processed")
                     continue
                 if str(inst) in set(keys):
                     new_name = format_string(abbr, f)
+                    print(new_name)
                     os.rename(f, new_name)
         except:
             print("Couldn't handle file {}".format(f))
