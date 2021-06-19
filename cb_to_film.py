1af732c4bae19421bcf2768949f87674198ccb87
Hunter <hansena@mit.edu>
2021-06-19 13:23:16 -0400

    Adding cubase_to_film lookup table

---
+++ cb_to_film.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+import fnmatch, string
+
+#List of instrument keys
+
+abbr_dict = {
+    ('pic', 'fl', 'afl', 'ob', 'eh', 'cl', 'bcl', 'bsn') : 'WW', 
+    ('fh', 'tpt', 'tbn', 'btbn', 'tb') : 'BRS',
+    ('timp', 'sn', 'cym', 'gc', 'bd', 'xyl', 'mrb', 'glk', 'vib', 'tbls', 'hprc', 'sprc') : 'PRC',
+    ('pno', 'clst', 'epno', 'org', 'hrp') : 'KBHRP',
+    ('vln', 'vla', 'vc', 'cb') : 'STR',
+    ('svx', 'choir') : 'VC',
+    ('egt', 'agt') : 'GUIT',
+    ('kd', 'sn', 'cym', 'hh', 'tom', 'loop') : 'DRUMS',
+    ('ebs', 'sbs', 'cb') : 'BASS',
+    ('synth') : 'SYN'
+}
+
+def format_string(abbr, filename):
+    bp = filename.split("_")
+    return bp[0] + "_{}_".format(abbr) + bp[1]
+
+def main():
+    files = [f for f in os.listdir(os.getcwd()) if fnmatch.fnmatch(f, '[!_]*')]
+    for f in files:
+        inst = f.split('_')[1].split('.')[0].translate(None, string.digits)
+        for keys, abbr in abbr_dict.items():
+            if str(inst) in set(keys):
+                new_name = format_string(abbr, f)
+                os.rename(f, new_name)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
--- software_setup.bash
+++ software_setup.bash
@@ -1,17 +1,22 @@
 # Installing brew package manager
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Installing Handbrake video/audio encoder
-brew install handbrake
+# # Installing Handbrake video/audio encoder
+# brew install handbrake
 
-# Installing ffmpeg command-line audio encoder
-brew install ffmpeg
+# # Installing ffmpeg command-line audio encoder
+# brew install ffmpeg
 
-# Installing youtube-dl youtube video downloader
-brew install youtube-dl
+# # Installing youtube-dl youtube video downloader
+# brew install youtube-dl
+
+# Inject Bash functions into .bashrc
 
 'function wav_to_mp3() {
 for i in *.wav;
   do ffmpeg -i "$i" -vn -ar 44100 -ac 2 -b:a 192k "${i%.wav}.mp3";
 done
+}
+
+alias cb_to_film="python ~/sofware_for_scoring/cb_to_film.py"
 }' >> ~/.bash_profile
